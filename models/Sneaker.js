f3cd940c76942c304f047a4f6ec691cfd41efd0a
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-11-14 11:43:08 +0100

    "route home created"

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -9,7 +9,7 @@ const sneakerSchema = new Schema({
     price: Number,
     category: {
         type: String,
-        enum: [men, women, kids]
+        enum: ['men', 'women', 'kids']
     },
     id_tags: {
         type: Schema.types.ObjectId,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/home", (req, res) => {
   res.render("index");
 });
+
+
 
 router.get("/sneakers/:cat", (req, res) => {
   res.render('products');
