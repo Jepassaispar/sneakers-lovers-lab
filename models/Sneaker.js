7b4229cf431ed0393fed3a60dfbfc069d502c59e
Nicolas BISCHOFF <nicolasbischoff@MacBook-Pro-de-Nicolas.local>
2019-11-14 11:45:16 +0100

    tag created

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema();
 
 const sneakerSchema = new Schema({
-    name: String,
-    ref: String,
-    sizes: [Number],
-    description: String,
-    price: Number,
-    category: {
-        type: String,
-        enum: [men, women, kids]
-    },
-    id_tags: {
-        type: Schema.types.ObjectId,
-        ref: 'tag'
-    }
-})
+  name: String,
+  ref: String,
+  sizes: [Number],
+  description: String,
+  price: Number,
+  category: {
+    type: String,
+    enum: ["men", "women", "kids"]
+  },
+  id_tags: {
+    type: Schema.types.ObjectId,
+    ref: "tag"
+  }
+});
 
-const sneakerModel = mongoose.model('Sneaker', sneakerSchema);
+const sneakerModel = mongoose.model("Sneaker", sneakerSchema);
 
 module.exports = sneakerModel;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/home", (req, res) => {
   res.render("index");
 });
 
 router.get("/sneakers/:cat", (req, res) => {
-  res.render('products');
+  res.render("products");
 });
 
 router.get("/one-product/:id", (req, res) => {
