42e4128e45de8f7ad0f3bea5e05c9388281e3fb3
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-11-14 13:45:42 +0100

    model sneakers updated

--- models/Sneaker.js
+++ models/Sneaker.js
@@ -2,19 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema();
 
 const sneakerSchema = new Schema({
-  name: String,
-  ref: String,
-  sizes: [Number],
-  description: String,
-  price: Number,
-  category: {
-    type: String,
-    enum: ["men", "women", "kids"]
-  },
-  id_tags: {
-    type: Schema.types.ObjectId,
-    ref: "tag"
-  }
+    name: String,
+    ref: String,
+    sizes: [Number],
+    description: String,
+    price: Number,
+    category: [{
+            men: Boolean
+        },
+        {
+            women: Boolean
+        },
+        {
+            kids: Boolean
+        }
+    ],
+    id_tags: {
+        type: Schema.types.ObjectId,
+        ref: "tag"
+    }
 });
 
 const sneakerModel = mongoose.model("Sneaker", sneakerSchema);
